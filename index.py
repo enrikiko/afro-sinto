5d76fcb9fe5b833b65456fa3d1a8359431da8c7b
enrikiko_91 <enrikiko_91@hotmail.com>
2018-09-19 23:13:23 +0200

    Button select

--- index.py
+++ index.py
@@ -3,11 +3,14 @@ from time import sleep
 #open the browser and set the url
 url = 'https://www.instagram.com/'
 from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 # browser = webdriver.Chrome()
 # browser.set_window_position(0, 0)
 # browser.set_window_size(1024, 768)
 # browser.get(url)
 # sleep(1)
-browser = webdriver.Firefox(executable_path="D:\\geckodriver.exe")
+browser = webdriver.Firefox(executable_path="./geckodriver")
 browser.get(url)
+browser.set_window_position(0, 0)
+browser.set_window_size(1024, 768)
+sleep(1)
